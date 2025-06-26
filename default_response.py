f596af35fb4639e934c6cb8882354ebd520c9c4e
Raahguu <raahguu@outlook.com>
2025-06-27 01:00:28 +0800

    Now has GET funcionality, to get all allowed files on the server

--- default_response.py
+++ default_response.py
@@ -6,13 +6,35 @@ Content-length: 21
 Error 400 Bad Request""".replace(b"\n", b"\r\n")
 
 ERRNO404 = b"""\
+HTTP/1.1 403 Forbidden
+Content-type: test/plain
+Content-length: 19
+
+Error 403 Forbidden""".replace(b"\n", b"\r\n")
+
+ERRNO404 = b"""\
 HTTP/1.1 404 Not Found
 Content-type: test/plain
 Content-length: 24
 
 Error 404 Page Not Found""".replace(b"\n", b"\r\n")
 
+ERRNO405 = b"""\
+HTTP/1.1 405 Method Not Allowed
+Content-type: text/plain
+Content-length: 27
+
+Error 405 Method Not Allowed""".replace(b"\n", b"\r\n")
+
+
 HELLO_WORLD = b"""\
 HTTP/1.1 200 OK
 
 Hello, World!""".replace(b"\n", b"\r\n")
+
+FILE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace("\n", "\r\n")
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import socket
 import default_response as dr
+import os
 
 HOST = "127.0.0.1"
-PORT = 80
+PORT = 8080
 MAX_REQUEST_LINE_LENGTH = 5
 
 class Request():
@@ -12,11 +13,11 @@ class Request():
 	headers: list[str]
 
 	@classmethod
-	def from_socket(cls, client_socket : socket.socket) -> 'Request':
+	def from_socket(cls, client_sock: socket.socket) -> 'Request':
 		try:
-			request_data = client_socket.recv(1024).decode('utf-8')
+			request_data = client_sock.recv(1024).decode('utf-8')
 			data = [line for line in request_data.splitlines()]
-			first_line = data[0].split()
+			first_line = data[0].split(' ')
 			request = Request()
 			request.method = first_line[0]
 			request.path = first_line[1]
@@ -25,14 +26,43 @@ class Request():
 			return request
 		except Exception as e:
 			print(f"Failed to parse request: {e}")
-			client_socket.sendall(dr.ERRNO400)
+			client_sock.sendall(dr.ERRNO400)
 			
+def serve_GET(sock: socket.socket, request: Request):
+	if request.method != "GET": 
+		raise TypeError(f"serve_GET only handles GET requests, not {request.method} requests")
+	path = request.path
+	if path == "/": path = "/index.html"
+	# Throw an error, if the path could be malicious
+	if ".." in path: 
+		sock.sendall(dr.ERRNO400)
+		return False
+	
+	# Make all the files they can access need to be in the 'content' folder
+	path = "content" + path
+	# If the path they specify does not have a file type in it, add html to the end
+	if "." not in path: path += ".html"
+	try:
+		with open(path, "rb") as file:
+			stat = os.stat(path)
+			headers = dr.FILE_TEMPLATE.format(content_type="text/html", 
+											 content_length=stat.st_size).encode('utf-8')
+			sock.sendall(headers)
+			sock.sendfile(file)
+	except (FileNotFoundError, IsADirectoryError) as e:
+		print(e)
+		sock.sendall(dr.ERRNO404)
+	except PermissionError:
+		sock.sendall(dr.ERRNO403)
+
 
 def handle_request(client_sock: socket.socket):
 	request = Request.from_socket(client_sock)
-	client_sock.sendall(dr.HELLO_WORLD)
-	# Remember to close, or the client will wait forever
-	client_sock.close()
+	# Only GET is implemented yet
+	if request.method != "GET":
+		client_sock.sendall(dr.ERRNO405)
+		return False
+	serve_GET(client_sock, request)
 		
 
 def serve_forever():
@@ -49,6 +79,7 @@ def serve_forever():
 	while True:
 		client_sock, client_addr = server.accept()
 		handle_request(client_sock)
+		client_sock.close() # Need to close so their browser page loads
 	
 if __name__ == "__main__":
 	serve_forever()
